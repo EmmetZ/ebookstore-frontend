0b45177d46b8609fcb3fb2628b4e1dce62976762
EmmetZ <Emmet_Z@outlook.com>
2025-03-13 15:01:27 +0800

    feat: add avatar

---
+++ src/components/avatar.tsx
@@ -0,0 +1,29 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar } from "antd";
+import React from "react";
+import { User } from "../types";
+
+interface AvatarProps {
+  user: User;
+  size?: number;
+}
+
+const UserAvatar: React.FC<AvatarProps> = ({ user, size }) => {
+  return (
+    <>
+      {user.avatar ? (
+        <Avatar src={user.avatar} size={size}/>
+      ) : (
+        <Avatar
+          icon={<UserOutlined style={{ color: "black" }} />}
+          style={{
+            backgroundColor: "white",
+            border: "1px solid gray",
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+export default UserAvatar;
